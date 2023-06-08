62365ee7b999d7074e6f69dca16e7005943ad8d9
장진태 <@DESKTOP-BNQ0M62>
2023-06-08 21:54:02 +0900

    ECI2ECEF_DCM for GPS satellite update

--- ECI2ECEF_DCM_GPS.m
+++ ECI2ECEF_DCM_GPS.m
@@ -1,7 +1,7 @@
-function gps_DCM=ECI2ECEF_DCM_GPS(time) %[YYYY,MM,DD,hh,mm,ss]
-time_dt=datetime(time);
-time_jd=juliandate(time_dt);
-time_GMST=siderealTime(time_jd);
+function gps_DCM=ECI2ECEF_DCM_GPS(time_gps) %[YYYY,MM,DD,hh,mm,ss]
+time_dt_gps=datetime(time_gps);
+time_jd_gps=juliandate(time_dt_gps);
+time_GMST_gps=siderealTime(time_jd_gps);
 
 gps_UT=[2023, 4, 29, 0, 0, 0]; %Universal time on Apr 29, 2023
 gps_UT_dt=datetime(gps_UT);
@@ -10,7 +10,7 @@ gps_UT_GMST=siderealTime(gps_UT_jd);
 
 w_earth=360/86160; %earth angular rate [deg/s], sidereal day in seconds
 
-gps_t_t0=etime(time,gps_UT); %[s]
+gps_t_t0=etime(time_gps,gps_UT); %[s]
 
 gps_theta_g=gps_UT_GMST+w_earth*gps_t_t0;
 
