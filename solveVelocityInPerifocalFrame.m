a6811fc7cb94e23eee65381881c29c1151a6d2f8
장진태 <@DESKTOP-BNQ0M62>
2023-06-08 22:03:05 +0900

    function solveVelocityInPerifocalFrame

--- solveVelocityInPerifocalFrame.m
+++ solveVelocityInPerifocalFrame.m
@@ -1,5 +1,5 @@
-function velocityInPQW = solveVelocityInPerifocalFrame(semimajor_axis, eccentricity, true_anomaly) %input unit-> [deg] & [km]
-p=semimajor_axis*(1-eccentricity^2); %[km]
+function velocityInPQW = solveVelocityInPerifocalFrame(semimajor_axis, eccentricity, true_anomaly) %input unit-> [rad] & [m]
+p=semimajor_axis*(1-eccentricity^2); %[m]
 u=3.986004418*10^5; %[km^3 s^-2]
 
 velocityInPQW=sqrt(u/p)*[-sin(true_anomaly*(pi/180)); eccentricity+cos(true_anomaly*(pi/180)); 0]; %[km/s]
