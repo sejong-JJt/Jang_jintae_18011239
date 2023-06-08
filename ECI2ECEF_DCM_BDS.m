c0befeb2e58b524bdf83c8c04c1cd281af7139bf
장진태 <@DESKTOP-BNQ0M62>
2023-06-08 21:53:24 +0900

    ECI2ECEF_DCM for BDS satellite update

--- ECI2ECEF_DCM_BDS.m
+++ ECI2ECEF_DCM_BDS.m
@@ -1,16 +1,16 @@
-function bds_DCM=ECI2ECEF_DCM_BDS(time) %[YYYY,MM,DD,hh,mm,ss]
-time_dt=datetime(time);
-time_jd=juliandate(time_dt);
-time_GMST=siderealTime(time_jd);
+function bds_DCM=ECI2ECEF_DCM_BDS(time_bds) %[YYYY,MM,DD,hh,mm,ss]
+time_dt_bds=datetime(time_bds);
+time_jd_bds=juliandate(time_dt_bds);
+time_GMST_bds=siderealTime(time_jd_bds);
 
-bds_UT=[2023, 5, 9, 2, 0, 0]; %Universal time on Apr 29, 2023
+bds_UT=[2023, 2, 26, 0, 0, 0]; %Universal time on Apr 29, 2023
 bds_UT_dt=datetime(bds_UT);
 bds_UT_jd=juliandate(bds_UT_dt);
 bds_UT_GMST=siderealTime(bds_UT_jd); 
 
 w_earth=360/86160; %earth angular rate [deg/s], sidereal day in seconds
 
-bds_t_t0=etime(time,bds_UT); %[s]
+bds_t_t0=etime(time_bds,bds_UT); %[s]
 
 bds_theta_g=bds_UT_GMST+w_earth*bds_t_t0;
 
